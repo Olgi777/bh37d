c7d6a06f123cfdc0888b95f6705d88a18884039c
Olgi777 <ochumak1985@gmail.com>
2023-02-06 10:14:24 +0300

    Revert "add print('qqqq')     print('qqqq')     print('qqqq')"
    This reverts commit e38a3d0d

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 print('hello world') 
 print('qqqq')
 print('qqqq')
-print("python")
-print("python")
-print("python")
-print("python")
-print("python")
+print('qqqq')
+print('qqqq')
+print('qqqq')
+print('qqqq')
+print('qqqq')
 print("python")
 
 
