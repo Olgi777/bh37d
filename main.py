38cd5d3e4169680381a366ef0dad6997c718f498
Olgi777 <ochumak1985@gmail.com>
2023-02-08 09:49:10 +0300

    Homework_1_

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# Заполнить список степенями числа 2 (от 2^1 до 2^n)
-
-result = []
-n = 0
-n = int(input("Введите количество раз возведений в степень: "))
-for i in range(n):
-    result = 2 ** (i+1)
-    print(result)
+print('hello world')
 
 
