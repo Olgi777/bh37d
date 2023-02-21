a3e55921f5eb4cabc15dd16fdc8bc16bd6d04602
Olgi777 <ochumak1985@gmail.com>
2023-02-21 10:30:07 +0300

    Homework___9_lesson5.py1_lesson4.py

--- Homework___9_lesson5.py
+++ Homework___9_lesson5.py
@@ -2,3 +2,14 @@
 # словарь с данными о пользователе (имя, фамилия, телефон, почта), вывести
 # имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
 # пустая строка)
+
+students = {"id_1": {"name": "Olya", "surname": "Ivanova", "phone": "234568", "email": "olya@info.com"},
+            "id_2": {"name": "Dmitri", "surname": "Petrov", "phone": "523789", "email": " "},
+            "id_3": {"name": "Katya", "surname": "Goncharova", "phone": "369258", "email": "katua@info.com"},
+            "id_4": {"name": "Evgeni", "surname": "Potapov", "phone": "741852", " ": "evgeni@info.com"}
+            }
+for student in students:
+    print(students[student])
+    for id in students(key):
+        if "email" == " " or "email" not in :
+            print("name")
