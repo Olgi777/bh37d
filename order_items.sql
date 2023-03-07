9f0e83e4c970c00e1345fc7f9e5f7ad85fc45e9d
Olgi777 <ochumak1985@gmail.com>
2023-03-07 14:13:58 +0300

    categories.sql order_items.sql orders.sql products.sql statuses.sql users.sql

--- order_items.sql
+++ order_items.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS order_items(
     id INTEGER SERIAL PRIMARY KEY,
-    order_id INTEGER FOREIGN KEY,
-    product_id INTEGER FOREIGN KEY,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
 );
--- products.sql
+++ products.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS products(
     id INTEGER SERIAL PRIMARY KEY,
     title VARCHAR(36) NOT NULL,
     description VARCHAR(140) NOT NULL,
-    category_id INTEGER FOREIGN KEY;
+    category_id INTEGER NOT NULL;
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
 
 );
